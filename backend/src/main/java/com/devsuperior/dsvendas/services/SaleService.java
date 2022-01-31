89759e1f5573faedfb9e563f15930af18b72e223
Alexandre Estevão <alexandresestevao@gmail.com>
2022-01-31 11:58:26 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SaleService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dsvendas.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository saleRepository;
+	
+	@Autowired
+	private SellerRepository sallerRepository;
+	
+	@Transactional(readOnly = true) // Garante que toda transação se resolida no service e "reakOnly" não deixar fazer lock no banco
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		sallerRepository.findAll();
+		Page<Sale> result = saleRepository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+
+}
+
+
