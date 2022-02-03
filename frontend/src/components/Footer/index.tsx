63eb54deeb8924d1eb6fd19a7d32402e5fd3a977
Alexandre Estevão <alexandresestevao@gmail.com>
2022-02-03 10:23:52 -0300

    Update footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,8 +3,7 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/alexandreestevao" target="_blank" rel="noreferrer">Alexandre Estevão</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                <p className="text-light"><small><strong>Spring React</strong><br /></small></p>
             </div>
         </footer>
     );
